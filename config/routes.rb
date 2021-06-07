1c70d583ff2998c086d8eab3cf19ca44d4536e03
zaryanz <aryanx404@gmail.com>
2021-06-07 22:28:21 +0530

    added milligram and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'home/about'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
