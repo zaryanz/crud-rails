4a68d3fefe5b6b30403f1c96b2b8768ae19e40b8
zaryanz <aryanx404@gmail.com>
2021-06-09 12:52:17 +0530

    crud functionality and foundation styles

---
+++ db/migrate/20210607173519_create_people.rb
@@ -0,0 +1,11 @@
+class CreatePeople < ActiveRecord::Migration[5.2]
+  def change
+    create_table :people do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
